108de42254784f1cb41b738ac69faa3ba620f536
waynee95 <larsericmarquardt@mail.de>
2021-06-06 23:32:38 +0200

    Fix scope handling for pattern

--- src/main/java/me/waynee95/rift/scope/ScopeVisitor.java
+++ src/main/java/me/waynee95/rift/scope/ScopeVisitor.java
@@ -1,51 +1,60 @@
 package me.waynee95.rift.scope;
 
 import me.waynee95.rift.ast.Visitor;
-import me.waynee95.rift.ast.node.Constructor;
-import me.waynee95.rift.ast.node.Let;
-import me.waynee95.rift.ast.node.Match;
+import me.waynee95.rift.ast.node.*;
 import me.waynee95.rift.ast.node.Record;
 import me.waynee95.rift.ast.node.decl.*;
-import me.waynee95.rift.ast.node.pattern.ConstructorPattern;
-import me.waynee95.rift.ast.node.pattern.MatchCase;
+import me.waynee95.rift.ast.node.pattern.*;
 import me.waynee95.rift.ast.node.reference.FuncCall;
 import me.waynee95.rift.ast.node.reference.Name;
 import me.waynee95.rift.ast.node.type.TCustom;
 import me.waynee95.rift.ast.node.type.TypeLit;
 import me.waynee95.rift.error.RiftException;
 
+import java.util.List;
 import java.util.Optional;
 
 public class ScopeVisitor implements Visitor<Scope> {
 
-    public void visit(Let node, Scope ctx) {
+    @Override
+    public void visitLet(Let node, Scope ctx) {
         Scope current = new Scope(ctx);
         visitOthers(node, current);
     }
 
-    public void visit(VarDecl node, Scope ctx) {
+    @Override
+    public void visitVarDecl(VarDecl node, Scope ctx) {
         ctx.declare(node.id, node);
         visitOthers(node, ctx);
     }
 
-    public void visit(FuncDecl node, Scope ctx) {
+    @Override
+    public void visitFuncDecl(FuncDecl node, Scope ctx) {
         ctx.declare(node.id, node);
         Scope functionScope = new Scope(ctx);
         visitOthers(node, functionScope);
     }
 
-    public void visit(RecordTypeDecl node, Scope ctx) {
+    @Override
+    public void visitExternDecl(ExternDecl node, Scope ctx) {
         ctx.declare(node.id, node);
     }
 
-    public void visit(Name node, Scope ctx) {
+    @Override
+    public void visitRecordTypeDecl(RecordTypeDecl node, Scope ctx) {
+        ctx.declare(node.id, node);
+    }
+
+    @Override
+    public void visitName(Name node, Scope ctx) {
         Optional<Decl> decl = ctx.lookup(node.id);
         if (decl.isEmpty()) {
             throw new RiftException("Undefined variable: " + node.id, Optional.of(node));
         }
     }
 
-    public void visit(FuncCall node, Scope ctx) {
+    @Override
+    public void visitFuncCall(FuncCall node, Scope ctx) {
         Optional<Decl> decl = ctx.lookup(node.id);
         if (decl.isEmpty()) {
             throw new RiftException("Undefined function: " + node.id, Optional.of(node));
@@ -53,55 +62,84 @@ public class ScopeVisitor implements Visitor<Scope> {
         visitOthers(node, ctx);
     }
 
-    public void visit(Record node, Scope ctx) {
+    @Override
+    public void visitRecord(Record node, Scope ctx) {
         Optional<Decl> decl = ctx.lookup(node.id);
         if (decl.isEmpty()) {
             throw new RiftException("Undefined record type: " + node.id, Optional.of(node));
         }
     }
 
-    public void visit(EnumTypeDecl node, Scope ctx) {
+    @Override
+    public void visitEnumTypeDecl(EnumTypeDecl node, Scope ctx) {
         ctx.declare(node.id, node);
         for (VariantDecl variantDecl : node.constructors) {
             ctx.declare(variantDecl.id, variantDecl);
-            visit(variantDecl, ctx);
+            visitVariantDecl(variantDecl, ctx);
         }
     }
 
-    public void visit(VariantDecl node, Scope ctx) {
+    @Override
+    public void visitVariantDecl(VariantDecl node, Scope ctx) {
         for (TypeLit field : node.fields) {
             if (field instanceof TCustom) {
-                visit((TCustom) field, ctx);
+                field.accept(this, ctx);
             }
         }
     }
 
-    public void visit(Constructor node, Scope ctx) {
+    @Override
+    public void visitConstructor(Constructor node, Scope ctx) {
         Optional<Decl> decl = ctx.lookup(node.id);
         if (decl.isEmpty()) {
             throw new RiftException("Undefined constructor: " + node.id, Optional.of(node));
         }
     }
 
-    public void visit(TCustom node, Scope ctx) {
+    @Override
+    public void visitTCustom(TCustom node, Scope ctx) {
         Optional<Decl> decl = ctx.lookup(node.id);
         if (decl.isEmpty()) {
             throw new RiftException("Undefined custom type: " + node.id, Optional.of(node));
         }
     }
 
-    public void visit(Match node, Scope ctx) {
+    @Override
+    public void visitMatch(Match node, Scope ctx) {
         for (MatchCase matchCase : node.cases) {
-            var pattern = matchCase.pattern;
-            if (pattern instanceof ConstructorPattern) {
-                Optional<Decl> decl = ctx.lookup(((ConstructorPattern) pattern).id);
-                if (decl.isEmpty()) {
-                    throw new RiftException(
-                            "Undefined constructor: " + ((ConstructorPattern) pattern).id,
-                            Optional.of(node));
-                }
-            }
+            matchCase.accept(this, ctx);
         }
+    }
+
+    @Override
+    public void visitMatchCase(MatchCase node, Scope ctx) {
+        Scope matchCaseScope = new Scope(ctx);
+        node.pattern.accept(this, ctx);
+        node.body.accept(this, ctx);
+    }
+
+    @Override
+    public void visitConstructorPattern(ConstructorPattern pattern, Scope ctx) {
+        Optional<Decl> decl = ctx.lookup(pattern.id);
+        if (decl.isEmpty()) {
+            throw new RiftException("Undefined constructor: " + pattern.id, Optional.of(pattern));
+        }
+        for (Pattern field : pattern.fields) {
+            field.accept(this, ctx);
+        }
+    }
+
+    @Override
+    public void visitRecordPattern(RecordPattern pattern, Scope ctx) {
+        List<VarDecl> fields = pattern.fields;
+        for (VarDecl field : fields) {
+            ctx.declare(field.id, field);
+        }
+    }
+
+    @Override
+    public void visitVariablePattern(VariablePattern node, Scope ctx) {
+        ctx.declare(node.name.id, node.name);
     }
 }
 
