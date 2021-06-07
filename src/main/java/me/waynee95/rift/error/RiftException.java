9b349cb13430d2d3eb4670ff362f7c6a94bcf173
waynee95 <larsericmarquardt@mail.de>
2021-06-08 00:34:02 +0200

    Fix column number in error message

--- src/main/java/me/waynee95/rift/error/RiftException.java
+++ src/main/java/me/waynee95/rift/error/RiftException.java
@@ -1,27 +1,25 @@
 package me.waynee95.rift.error;
 
-import me.waynee95.rift.ast.Node;
-
-import java.util.Optional;
+import me.waynee95.rift.ast.node.Node;
 
 public class RiftException extends RuntimeException {
-    public final Optional<Node> node;
+    public final Node node;
 
-    public RiftException(String message, Optional<Node> node) {
+    public RiftException(String message, Node node) {
         super(message);
         this.node = node;
     }
 
     public RiftException(String message) {
         super(message);
-        this.node = Optional.empty();
+        this.node = null;
     }
 
     @Override
     public String getMessage() {
-        if (node.isEmpty()) {
+        if (node == null) {
             return super.getMessage();
         }
-        return "(" + node.get().getLine() + ":" + node.get().getCol() + ") " + super.getMessage();
+        return "(" + node.getLine() + ":" + node.getCol() + ") " + super.getMessage();
     }
 }
